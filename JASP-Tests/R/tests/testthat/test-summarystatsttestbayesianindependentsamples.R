0d668613f3b15425b5a4cd1df48a30b3f4526f06
Alexandra Sarafoglou <alexandra.sarafoglou@student.uva.nl>
2019-08-05 10:05:16 +0200

    integrate jasp results for independent samples ttest

---
+++ JASP-Tests/R/tests/testthat/test-summarystatsttestbayesianindependentsamples.R
@@ -0,0 +1,33 @@
+context("Summary Statistics Bayesian Independent Samples T-Test")
+
+test_that("Main table results match", {
+  set.seed(0)
+  options <- analysisOptions("SummaryStatsTTestBayesianIndependentSamples")
+  options$tStatistic      <- 2.3
+  options$n1Size          <- 10
+  options$n2Size          <- 13
+  options$bayesFactorType <- "LogBF10"
+  results <- jasptools::run("SummaryStatsTTestBayesianIndependentSamples", "debug.csv", options)
+  
+  table <- results[["results"]][["bayesianIndependentTTestTable"]][["data"]]
+  expect_equal_tables(table, list(0.816974431461627, 2.3, 10, 0.000287271873137224, 0.0318022759865702,
+                                  13))
+})
+
+# test_that("Prior posterior plot matches and BF robustness check plot matches", {
+#   # with additional info
+#   set.seed(0)
+#   options <- analysisOptions("SummaryStatsTTestBayesianIndependentSamples")
+#   options$tStatistic            <- 2.3
+#   options$n1Size                <- 10
+#   options$n2Size                <- 13
+#   options$bayesFactorType           <- "LogBF10"
+#   options$plotPriorAndPosterior     <- TRUE
+#   options$plotBayesFactorRobustness <- TRUE
+#   results <- jasptools::run("SummaryStatsTTestBayesianIndependentSamples", "debug.csv", options)
+#   testPlot <- results[["results"]][["inferentialPlots"]][["BFrobustnessPlot"]][["data"]]
+#   expect_equal_plots(testPlot, "prior-posterior", dir="TTestBayesianIndependentSamples")
+#   
+#   # without additional info
+# })
+
